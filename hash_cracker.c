f4eed67b8c2d0aa42c50383ac7a25f1efc74a588
shawnthestudent <90001513+shawnthestudent@users.noreply.github.com>
2022-04-08 12:22:58 -0400

    latest

---
+++ hash_cracker.c
@@ -0,0 +1,9 @@
+// just enough so that it compiles...
+
+// put you hash_cracker here in place of this file.
+
+// You will need to remove the MakeSha256 subroutine, though.  It will now come from a different file
+
+int main(int argc, char *argv[]) {
+   return(0);
+}
